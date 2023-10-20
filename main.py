fb718ec37c54bdad07c95e7a97640bb268dd692a
Martin Mészáros <hdsektor38@gmail.com>
2023-10-20 13:16:14 +0200

    stop2

--- main.py
+++ main.py
@@ -3,7 +3,22 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 
 agazatok = ['Erősáramú\nelektrotechnikus', 'Ipari \ninformatikai\n technikus', 'Szoftverfejlesztő \nés -tesztelő', 'Informatikai \nrendszer- és\n alkalmazás-üzemeltető']
+agazatall = []
+with open('data.csv') as f:
+    for line in f:
+        inner = [data.strip() for data in line.split(',')]
+        agazatall.append(inner)
+
+
+
+
+for items in agazatall:
+    for item in items:
+        print(item)
+
 counts = [40, 100, 30, 55]
+
+
 bar_labels = ['Erősáramú elektrotechnikus', 'Ipari informatikai technikus', 'Szoftverfejlesztő és -tesztelő', 'Informatikai rendszer- és alkalmazás-üzemeltető']
 bar_colors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange']
 
